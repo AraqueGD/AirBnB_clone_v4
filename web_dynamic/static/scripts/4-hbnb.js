e4d0e46d00f9c09ad0bcd9e5a2ba414232772d11
Nzparra <nzparra@gmail.com>
2020-05-17 16:43:50 -0500

    [Fix]Filter button

--- web_dynamic/static/scripts/4-hbnb.js
+++ web_dynamic/static/scripts/4-hbnb.js
@@ -13,7 +13,6 @@ $(document).ready(function () {
             $('div.amenities > h4').html('&nbsp;');
         }
     });
-});
 $.get('http://0.0.0.0:5001/api/v1/status/', function (data, textStatus) {
     if (textStatus === 'success') {
         if (data.status === 'OK') {
@@ -47,25 +46,9 @@ $.ajax({
     success: function(data){
         for (let i = 0; i < data.length; i++){
             let place = data[i];
-            $('.places ').append('<article><h2>' + place.name + '</h2><div class="price_by_night"><p>$' + place.price_by_night + '</p></div><div class="information"><div class="max_guest"><div class="guest_image"></div><p>' +  place.max_guest + '</p></div><div class="number_rooms"><div class="bed_image"></div><p>' + place.number_rooms + '</p></div><div class="number_bathrooms"><div class="bath_image"></div><p>' + place.number_bathrooms + '</p></div></div><div class="description"><p>' + place.description + '</p></div></article>');
+            $('.places ').append('<article><h2>' + place.name + '</h2><div class="price_by_night"><p>$' + place.price_by_night + '</p></div><div class="information"><div class="max_guest"><div class="guest_image"></div><p>' + place.max_guest + '</p></div><div class="number_rooms"><div class="bed_image"></div><p>' + place.number_rooms + '</p></div><div class="number_bathrooms"><div class="bath_image"></div><p>' + place.number_bathrooms + '</p></div></div><div class="description"><p>' + place.description + '</p></div></article>');
         }
     }
 });
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+});
 });
